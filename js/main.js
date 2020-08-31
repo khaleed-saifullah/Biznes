b89b5b423940514338d2e36dafe142804a6e2aa9
Khaleed <ksnayem@gmail.com>
2020-08-31 21:09:47 +0600

    project complete

---
+++ js/main.js
@@ -0,0 +1,59 @@
+$(document).ready(function () {
+    $('.portfolio-list').isotope({
+        layoutMode: 'masonry',
+        masonry: {
+            columnWidth: '.col-md-4',
+            horizontalOrder: false
+        }
+    });
+
+    $('.portfolio-filter li').on('click', function () {
+        $('.portfolio-filter li').removeClass('active');
+        $(this).addClass('active');
+
+        var filterValue = $(this).attr('data-filter');
+
+        $('.portfolio-list').isotope({
+            filter: filterValue,
+            layoutMode: 'masonry',
+            masonry: {
+                columnWidth: '.col-md-4',
+                horizontalOrder: false
+            }
+        });
+    });
+
+    $(window).scroll(function () {
+        var ourWindow = $(this).scrollTop();
+
+        if (ourWindow < 500) {
+            $('.top-arrow').fadeOut();
+        } else {
+            $('.top-arrow').fadeIn();
+        }
+
+        if (ourWindow > 100) {
+            $('body').addClass('fixed');
+        } else {
+            $('body').removeClass('fixed');
+        }
+    });
+
+    $('.top-arrow').hide();
+
+    $('.top-arrow').click(function () {
+        $("html,body").animate({
+            scrollTop: 0
+        }, 1000);
+    });
+
+    $('navbar-nav a').click(function (e) {
+        e.preventDefault();
+
+        var target = this.hash;
+
+        $("html,body").animate({
+            scrollTop: $(target).offset().top - 80
+        }, 1000);
+    });
+});
---
+++ readme.md
@@ -0,0 +1,29 @@
+
+# Biznes
+
+### This is a single page Bootstarp template
+
+
+## Live
+Demo [https://khaleed-saifullah.github.io/Biznes/]( https://khaleed-saifullah.github.io/Biznes/)
+
+
+## Built With
+
+* [HTML5](https://developer.mozilla.org/en-US/docs/Web/Guide/HTML/HTML5)
+* [CSS3](https://developer.mozilla.org/en-US/docs/Web/CSS/CSS3)
+* [Bootstrap](https://getbootstrap.com/docs/4.2/getting-started/introduction/)
+
+## What I have done
+
+* Raw Html
+* Raw CSS with Bootstrap
+* Fully Responsive
+
+# How to use
+* >$ git clone [repo-link]
+* browse index.html
+
+
+
+
